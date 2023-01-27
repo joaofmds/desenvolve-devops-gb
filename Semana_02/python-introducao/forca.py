d34e36e1081430a0b35e94e6b1bc54481ce800f3
joaofmds <ms.joao.felipe@gmail.com>
2023-01-26 22:18:43 -0300

    Modulariza os arquivos

---
+++ Semana_02/python-introducao/forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print('**************************')
+    print('Bem vindo ao jogo da Foca!')
+    print('**************************')
+
+    print('Fim do Jogo!')
+
+if __name__ == '__main__':
+    jogar()
