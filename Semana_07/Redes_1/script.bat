be624e861bb1481902db609d7941ab15cd4f9f2a
joaofmds <ms.joao.felipe@gmail.com>
2023-02-17 17:23:24 -0300

    Redes parte 1: conceitos e prática

---
+++ Semana_07/Redes_1/script.bat
@@ -0,0 +1,27 @@
+echo "A rede com IP, ping e traceroute"
+
+ping www.google.com.br
+tracert -d www.google.comC
+
+echo "DNS, hub e as conexões de máquinas"
+
+ping 192.168.3.1
+ping 127.0.0.1
+ping www.cursoderedesdaalura.com
+nslookup www.facebook.com
+nslookup x
+ping 1.1.1.1
+
+echo "Cabos de conexão"
+
+echo "Montando nossa rede no programa"
+
+echo "Switches e seus avanços"
+
+echo "Roteadores e a comunicação externa"
+
+echo "Entendendo os endereços IP"
+
+echo "Endereçamento IP com DHCP"
+
+echo "Acessando o site em nossa simulação"
